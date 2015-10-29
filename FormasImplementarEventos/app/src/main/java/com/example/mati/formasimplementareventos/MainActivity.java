81f87d294601fd45a2fe3a5cce552e7c81921e60
jmollaf <jmollaf@gmail.com>
2015-10-29 14:45:16 +0100

    TERMINADO EJERCICIO FORMAS IMPLEMENTAR EVENTOS

--- FormasImplementarEventos/app/src/main/java/com/example/mati/formasimplementareventos/MainActivity.java
+++ FormasImplementarEventos/app/src/main/java/com/example/mati/formasimplementareventos/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.mati.formasimplementareventos;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context ctx=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
+        ctx=this;
+        Button cmdTres = (Button)findViewById(R.id.cmdTres);
+        cmdTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ctx, "Pulsado boton Tres",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+   
+}
     public void cmdDos_click(View v) {
         Toast.makeText(this, "Pulsado botón Dos", Toast.LENGTH_SHORT).show();
     }
 }
+
