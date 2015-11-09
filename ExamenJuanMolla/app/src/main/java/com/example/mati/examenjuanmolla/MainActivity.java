cbfac14063e187b5d10049d8b41c26b4fbc30c91
jmollaf <jmollaf@gmail.com>
2015-11-09 12:13:55 +0100

    spinner basico

--- ExamenJuanMolla/app/src/main/java/com/example/mati/examenjuanmolla/MainActivity.java
+++ ExamenJuanMolla/app/src/main/java/com/example/mati/examenjuanmolla/MainActivity.java
@@ -1,37 +1,21 @@
 package com.example.mati.examenjuanmolla;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    final static String pizzas[] = {"Margarita", "Tres quesos", "Barbacoa", "Carbonara"};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Spinner miSpinner = (Spinner) findViewById(R.id.spinnerPizzas);
+        ArrayAdapter<String> miAdaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, pizzas);
+        miAdaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); /* MIRAR BIEN( es diferente al ListaView) */
+        miSpinner.setAdapter(miAdaptador);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
