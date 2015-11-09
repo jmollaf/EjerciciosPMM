691926aa3e7b4b0e0a30719a235b0b30db74d12a
jmollaf <jmollaf@gmail.com>
2015-11-09 13:14:08 +0100

    Inicializadas variables de controles

--- ExamenJuanMolla/app/src/main/java/com/example/mati/examenjuanmolla/MainActivity.java
+++ ExamenJuanMolla/app/src/main/java/com/example/mati/examenjuanmolla/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.mati.examenjuanmolla;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> miAdaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, pizzas);
         miAdaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         miSpinner.setAdapter(miAdaptador);
-
+        RadioButton radioLocal = (RadioButton) findViewById(R.id.radioLocal);
+        RadioButton radioDomicilio = (RadioButton) findViewById(R.id.radioDomicilio);
+        CheckBox chkGrande = (CheckBox) findViewById(R.id.checkBoxGrande);
+        CheckBox chkMas = (CheckBox) findViewById(R.id.checkBoxMas);
+        CheckBox chkMenos = (CheckBox) findViewById(R.id.checkBoxExtra);
+        TextView txtUnidades = (TextView) findViewById(R.id.textViewUnidades);
+        TextView txtTotal = (TextView) findViewById(R.id.textViewTotal);
 
     }
+   
 }
